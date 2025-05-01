5a8a826fa6718a3ce7a44a4b2d67e314564f77d0
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-05-01 14:45:46 -0300

    estou tentando

--- main.py
+++ main.py
@@ -5,19 +5,27 @@ lista = []
 dy = {}
 pvp = {}
 pl = {}
-roe = {}
-for c in df.index:
-    if df.loc[c]['dy'] > 0.07 and df.loc[c]['pvp'] < 1 and df.loc[c]['pl'] < 6 and df.loc[c]['pl'] > 0 and df.loc[c]['roe'] > 0.15:
-        lista.append(c)
-        dy[c] = float(f"{df.loc[c]['dy']:.2f}")
-        pvp[c] = float(f"{df.loc[c]['pvp']:.2f}")
-        pl[c] = float(f"{df.loc[c]['pl']:.2f}")
-        roe[c] = float(f"{df.loc[c]['roe']:.2f}")
+liq2m = {}
 app = Flask(__name__)
-
+@app.route('/main', methods=['POST'])
+def filtrar():
+    for c in df.index:
+        dyv = request.form.get('div')
+        if dyv == '':
+            dyv = 0
+        else:
+            dyv = float(dyv)
+        print(dyv)
+        if df.loc[c]['dy'] > dyv and df.loc[c]['pvp'] < 1 and df.loc[c]['pl'] < 6 and df.loc[c]['pl'] > 0 and df.loc[c]['liq2m'] > 1500000:
+            lista.append(c)
+            dy[c] = float(f"{df.loc[c]['dy']:.2f}")
+            pvp[c] = float(f"{df.loc[c]['pvp']:.2f}")
+            pl[c] = float(f"{df.loc[c]['pl']:.2f}")
+            liq2m[c] = float(f"{df.loc[c]['liq2m']:.2f}")
+    return render_template('analise.html', nomes=lista, dy=dy, pvp=pvp, pl=pl,liq2m=liq2m)
 @app.route('/analise.html')
 def analise():
-    return render_template('analise.html', nomes=lista, dy=dy, pvp=pvp, pl=pl,roe=roe)
+    return render_template('analise.html', nomes=lista, dy=dy, pvp=pvp, pl=pl,liq2m=liq2m)
 @app.route('/graficos.html')
 def grafico():
     return render_template('graficos.html')
