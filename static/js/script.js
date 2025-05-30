f9a64b6465b80f5ddf8da4541bc4dd46e3f45626
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-05-29 22:07:11 -0300

    fiz a calculadora

--- static/js/script.js
+++ static/js/script.js
@@ -7,12 +7,17 @@ function calcular(){
     tempo = document.getElementById('itempo').value
     tempo_periodo = document.getElementById('tempo').value
     inicio = Number(inicio)
-    juros = Number(juros)
+    juros = Number(juros)/100
     aporte = Number(aporte)
     tempo = Number(tempo)
-    res.innerHTML = `${inicio}`
     if (tempo_periodo == "Anos"){
-        resultado = (inicio + juros/100)**tempo
-        res.innerHTML = `${resultado}`
+        tempo *= 12
+        juros = (1 + juros)**(1/12) -1
+        resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
+        res.innerHTML = `R$ ${resultado.toFixed(2)}`
+
+    }else{
+        resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
+        res.innerHTML = `R$ ${resultado.toFixed(2)}`
     }
 }
