296778bd383d4c1d72c63c009c986a1ef84ba0df
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-05-31 22:22:35 -0300

    ajeitei a calculadora de juros compostos

--- static/js/script.js
+++ static/js/script.js
@@ -6,18 +6,38 @@ function calcular(){
     aporte = document.getElementById('aporte').value
     tempo = document.getElementById('itempo').value
     tempo_periodo = document.getElementById('tempo').value
+    tempo_periodo_juros = document.getElementById('tempo_j').value
     inicio = Number(inicio)
     juros = Number(juros)/100
     aporte = Number(aporte)
     tempo = Number(tempo)
-    if (tempo_periodo == "Anos"){
-        tempo *= 12
-        juros = (1 + juros)**(1/12) -1
-        resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
-        res.innerHTML = `R$ ${resultado.toFixed(2)}`
-
+    if (tempo == 0 || juros == 0 || inicio == 0){
+        res.innerHTML = 'Coloque um valor válido'
     }else{
-        resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
-        res.innerHTML = `R$ ${resultado.toFixed(2)}`
+        if (tempo_periodo == "Anos"){
+            tempo *= 12
+            if(tempo_periodo_juros == "Anual"){
+                juros = (1 + juros)**(1/12) -1
+                resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
+                resultado = resultado.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})
+                res.innerHTML = `R$ ${resultado}`
+            }else{
+                resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
+                resultado = resultado.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})
+                res.innerHTML = `R$ ${resultado}`
+            }
+
+        }else{
+            if (tempo_periodo_juros == "Mensal"){
+                resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
+                resultado = resultado.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})
+                res.innerHTML = `R$ ${resultado}`
+            }else{
+                juros = (1 + juros)**(1/12) -1
+                resultado = inicio * (1 + juros)**tempo + aporte * (((1 + juros)**tempo -1)/ juros)
+                resultado = resultado.toLocaleString('pt-BR', {minimumFractionDigits: 2, maximumFractionDigits: 2})
+                res.innerHTML = `R$ ${resultado}`
+            }
+        }
     }
 }
